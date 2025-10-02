6d69bb978ca4ae90e410f56b72a5dccc9757aa8a
ba9hh <zedtourheart@gmail.com>
2025-10-02 17:40:58 +0100

    first commit

--- server/client/src/order/Order.jsx
+++ server/client/src/order/Order.jsx
@@ -1,28 +1,16 @@
 import { useContext, useEffect, useState } from "react";
-import { useForm, useFieldArray, Controller } from "react-hook-form";
-import {
-  Button,
-  Dialog,
-  DialogTitle,
-  DialogContent,
-  Box,
-  TextField,
-  Typography,
-  CircularProgress,
-} from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { useForm, useFieldArray } from "react-hook-form";
+import { Button, CircularProgress } from "@mui/material";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { AuthContext } from "../AuthProvider";
 import { toast } from "react-toastify";
-import biscuit50 from "../assets/biscuit50.jpg";
 import Dyari from "../components/Dyari";
 import LoginRequiredDialog from "../components/LoginRequiredDialog";
-import dayjs from "dayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import "react-datepicker/dist/react-datepicker.css";
-import Pagination from "../components/Pagination";
+import OrderItem from "./OrderItem";
+import OrderSummary from "./OrderSummary";
+import ArticleDialog from "./ArticleDialog";
 
 const Order = () => {
   const { user } = useContext(AuthContext);
@@ -31,6 +19,7 @@ const Order = () => {
   const [openDialog, setOpenDialog] = useState({ open: false, index: null });
   const [isConnected, setIsConnected] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
   const LIMIT = 8;
@@ -61,7 +50,7 @@ const Order = () => {
 
   useEffect(() => {
     const fetchShopArticles = async () => {
-      setLoading(true);
+      setIsLoading(true);
       try {
         const response = await axios.get(
           `https://dyari.onrender.com/api/shop/${id}/articles?page=${page}&limit=${LIMIT}`
@@ -71,7 +60,7 @@ const Order = () => {
       } catch (error) {
         console.error("Error fetching shop articles:", error);
       } finally {
-        setLoading(false);
+        setIsLoading(false);
       }
     };
 
@@ -144,17 +133,6 @@ const Order = () => {
       setLoading(false);
     }
   };
-  const handlePrev = () => {
-    if (page > 1) {
-      setPage(page - 1);
-    }
-  };
-
-  const handleNext = () => {
-    if (page < totalPages) {
-      setPage(page + 1);
-    }
-  };
   return (
     <div className="flex justify-center w-full min-h-screen bg-white sm:bg-[#f2f2f2] pt-16 pb-8 sm:px-0 px-2">
       <Dyari />
@@ -163,166 +141,33 @@ const Order = () => {
         className="w-full max-w-[600px] bg-white p-6 rounded-lg border sm:border:0 sm:shadow-md"
       >
         {fields.map((field, index) => (
-          <div key={field.id} className="mb-2">
-            <Typography variant="subtitle2">Article {index + 1}</Typography>
-            <div className="relative flex items-center justify-center space-x-2 mt-8 ">
-              {!watchItems[index]?.image && (
-                <div className="absolute flex flex-col justify-center animate-wiggle w-full pl-6">
-                  <h1 className="text-2xl">
-                    <span className="text-sm hidden sm:block">
-                      (select article first)
-                    </span>
-                    👉
-                  </h1>
-                </div>
-              )}
-              <img
-                src={watchItems[index].image || biscuit50}
-                alt="article"
-                style={{ width: "25%", cursor: "pointer" }}
-                onClick={() => handleOpenDialog(index)}
-                className="z-10 relative"
-              />
-            </div>
-            <div className="flex gap-2 items-center w-full mt-4">
-              <div className="w-full">
-                <div className="flex gap-2 mt-2">
-                  <Controller
-                    name={`items.${index}.type`}
-                    control={control}
-                    render={({ field }) => (
-                      <TextField
-                        fullWidth
-                        label="Unité de vente"
-                        {...field}
-                        value={field.value || ""}
-                        InputProps={{
-                          readOnly: true,
-                        }}
-                      />
-                    )}
-                  />
-
-                  <Controller
-                    name={`items.${index}.price`}
-                    control={control}
-                    render={({ field }) => (
-                      <TextField
-                        type="number"
-                        fullWidth
-                        label="Prix (dt)"
-                        {...field}
-                        value={field.value || ""}
-                        InputProps={{
-                          readOnly: true,
-                        }}
-                      />
-                    )}
-                  />
-                </div>
-
-                <Controller
-                  name={`items.${index}.quantity`}
-                  control={control}
-                  rules={{ min: { value: 1, message: "Quantity must be > 0" } }}
-                  render={({ field }) => (
-                    <TextField
-                      type="number"
-                      fullWidth
-                      label="Quantité"
-                      {...field}
-                      error={!!errors.items?.[index]?.quantity}
-                      helperText={errors.items?.[index]?.quantity?.message}
-                      sx={{ mt: 2 }}
-                    />
-                  )}
-                />
-
-                <Typography variant="body2" sx={{ mt: 1 }}>
-                  Sous-total:{" "}
-                  {(watchItems[index].price || 0) *
-                    (watchItems[index].quantity || 0)}{" "}
-                  dt
-                </Typography>
-              </div>
-            </div>
-          </div>
+          <OrderItem
+            key={field.id}
+            index={index}
+            control={control}
+            errors={errors}
+            watchItems={watchItems}
+            handleOpenDialog={handleOpenDialog}
+          />
         ))}
 
         <Button
           variant="outlined"
           fullWidth
-          onClick={() => append({ type: "", price: 0, image: "", quantity: 0 })}
+          onClick={() =>
+            append({ type: "", price: 0, image: "", quantity: null })
+          }
           sx={{ mb: 2 }}
         >
           Ajouter un autre article
         </Button>
-        <div className="flex flex-col sm:flex-row gap-2">
-          <div className="flex-1">
-            <Controller
-              name="userPhoneNumber"
-              control={control}
-              rules={{
-                required: "Téléphone requis",
-                pattern: { value: /^[2459]\d{7}$/, message: "Numéro invalide" },
-              }}
-              render={({ field }) => (
-                <TextField
-                  fullWidth
-                  label="Votre numéro de téléphone"
-                  {...field}
-                  error={!!errors.userPhoneNumber}
-                  helperText={errors.userPhoneNumber?.message}
-                  sx={{ mb: 2 }}
-                />
-              )}
-            />
-          </div>
-          <div className="flex-1">
-            <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <Controller
-                name="date"
-                control={control}
-                defaultValue={null}
-                rules={{ required: "Date is required" }}
-                render={({ field }) => (
-                  <DatePicker
-                    label="Select a date"
-                    value={field.value}
-                    onChange={(newValue) => field.onChange(newValue)}
-                    minDate={today}
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        fullWidth
-                        error={!!errors.date}
-                        helperText={errors.date?.message}
-                      />
-                    )}
-                  />
-                )}
-              />
-              {errors.date && (
-                <Typography
-                  variant="caption"
-                  color="error"
-                  sx={{ marginLeft: "14px" }}
-                >
-                  {errors.date.message}
-                </Typography>
-              )}
-            </LocalizationProvider>
-          </div>
-        </div>
 
-        <Typography variant="h6" align="center" sx={{ mb: 2 }}>
-          Total:{" "}
-          {watchItems.reduce(
-            (sum, itm) => sum + (itm.price || 0) * (itm.quantity || 0),
-            0
-          )}{" "}
-          dt
-        </Typography>
+        <OrderSummary
+          control={control}
+          errors={errors}
+          watchItems={watchItems}
+          today={today}
+        />
 
         <Button
           type="submit"
@@ -335,51 +180,16 @@ const Order = () => {
         </Button>
       </form>
 
-      <Dialog
-        open={openDialog.open}
-        onClose={handleCloseDialog}
-        fullWidth
-        maxWidth="sm"
-      >
-        <DialogTitle>Sélectionner un article</DialogTitle>
-        <DialogContent>
-          {loading ? (
-            <Box display="flex" justifyContent="center" p={2}>
-              <CircularProgress />
-            </Box>
-          ) : (
-            <div className="columns-2 sm:columns-2 md:columns-3 gap-2">
-              {shopData?.map((item, idx) => (
-                <div
-                  key={idx}
-                  onClick={() =>
-                    selectImage(
-                      item.articleImage,
-                      item.articleType,
-                      item.articlePrice,
-                      item._id
-                    )
-                  }
-                  className="mb-2 break-inside-avoid cursor-pointer"
-                >
-                  <img
-                    src={item.articleImage}
-                    alt="article"
-                    style={{ width: "100%" }}
-                  />
-                </div>
-              ))}
-            </div>
-          )}
-          <Pagination
-            currentPage={page}
-            totalPages={totalPages}
-            onPrev={handlePrev}
-            onNext={handleNext}
-            isOrder={true}
-          />
-        </DialogContent>
-      </Dialog>
+      <ArticleDialog
+        openDialog={openDialog}
+        handleCloseDialog={handleCloseDialog}
+        shopData={shopData}
+        selectImage={selectImage}
+        loading={isLoading}
+        page={page}
+        setPage={setPage}
+        totalPages={totalPages}
+      />
 
       <LoginRequiredDialog
         open={isConnected}
